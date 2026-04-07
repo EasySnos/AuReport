d77fc539b8ad1ba19c0c6deac6519493d216cf53
EasySnos <pavelamirov2704@gmail.com>
2026-04-07 15:00:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -171,6 +171,7 @@ async def final_step(message: types.Message):
         f"Айди пользователя: {message.from_user.id}\n"
         f"Юз пользователя: {username}\n"
         f"Текст: {data.get('description')}\n"
+        f"Причина: {reason}\n
         f"Ссылка: {data.get('link')}\n"
         f"Доказательства: {data.get('docs')}"
     )
