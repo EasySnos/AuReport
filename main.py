1bf0e0ce622aa1e118e8a43bede57ce4cbd381d1
EasySnos <pavelamirov2704@gmail.com>
2026-04-12 00:55:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,15 +55,7 @@ def only_allowed(func):
     async def wrapper(message: types.Message, *args, **kwargs):
         if message.from_user.id not in ALLOWED_USERS:
             await message.answer(
-                "У вас отсутствует подписка ❌\n"
-                "Его можно приобрести у - @id8666734115\n\n"
-                "💰 Цены в USDT:\n"
-                "1 день: 1 USDT\n"
-                "7 дней: 3 USDT\n"
-                "1 месяц: 7 USDT\n"
-                "Навсегда: 10 USDT\n\n"
-                "Оплата через @CryptoBot\n\n"
-                "Удалено 39 едениц ресурса.\n"
+                "Скрыто 39 едениц ресурса.\n"
                 "Число обновляется каждые 2 часа 🔃",
                 reply_markup=ReplyKeyboardRemove()
             )
